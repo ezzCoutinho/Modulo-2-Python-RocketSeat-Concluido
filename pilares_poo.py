e644d8d1b996d3bed99e5b312871260e2042b148
ezzCoutinho <lammerlopes@hotmail.com>
2024-12-24 10:47:17 -0300

    learning in module two

--- pilares_poo.py
+++ pilares_poo.py
@@ -1,4 +1,4 @@
-# Exemplo e herança
+# Exemplo de herança
 print("\nExemplo de herança: ")
 class Animal:
   def __init__(self, nome):
@@ -18,11 +18,43 @@ class Cachorro(Animal):
 class Gato(Animal):
   def emitir_som(self):
     return "Miau"
-  
+
 cachorro = Cachorro("Fluky")
 gato = Gato("Preto")
 
-cachorro.andar()
-print(cachorro.emitir_som())
-gato.andar()
-print(gato.emitir_som())
+# Exemplo de polimorfismo.
+
+print("\nExemplo de polimorfismo:")
+animais = [cachorro, gato]
+
+for animal in animais:
+  print(f"O {animal.nome}, faz: {animal.emitir_som()}")
+
+# Exemplo de encapsulamento.
+
+class ContaBancaria:
+  def __init__(self, saldo):
+    self.__saldo = saldo # Atributo privado.
+  
+  def depositar(self, valor):
+    if valor > 0:
+      self.__saldo += valor 
+
+  def sacar(self, valor):
+    if valor > 0 and valor <= self.__saldo:
+      self.__saldo -= valor
+
+  def get_saldo(self):
+    return self.__saldo
+  
+conta = ContaBancaria(1000)
+print(f"\nSeu saldo é de: {conta.get_saldo()}")
+conta.depositar(500)
+print(f"\nSeu saldo é de: {conta.get_saldo()}")
+conta.sacar(-500)
+print(f"\nSeu saldo é de: {conta.get_saldo()}")
+conta.sacar(2000)
+print(f"\nSeu saldo é de: {conta.get_saldo()}")
+
+
+  
