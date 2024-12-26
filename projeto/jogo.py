341bd77ebb39a27d590e34d6e8844e8695dc78e3
ezzCoutinho <lammerlopes@hotmail.com>
2024-12-26 19:12:43 -0300

    learning in module two

--- projeto/jogo.py
+++ projeto/jogo.py
@@ -19,6 +19,17 @@ class Personagem:
   
   def exibir_detalhes(self):
     return f" Nome: {self.get_nome()} \n Vida: {self.get_vida()} \n Nivel: {self.get_nivel()}"
+
+  def receber_ataque(self, dano):
+    self.__vida -= dano
+    if self.__vida <= 0:
+      self.__vida = 0 
+  
+  def atacar(self, alvo):
+    dano = self.__nivel * 2
+    alvo.receber_ataque(dano)
+    print(f"\nO {self.get_nome()} está atacando {alvo.get_nome()} e causou: {dano} de dano! \n")
+    
   
 
 class Heroi(Personagem):
@@ -32,6 +43,11 @@ class Heroi(Personagem):
   
   def exibir_detalhes(self):
     return f"{super().exibir_detalhes()} \n Habilidade: {self.__habilidade}\n"
+
+  def ataque_especial(self, alvo):
+    dano = self.get_nivel() * 5 
+    alvo.receber_ataque(dano)
+    print(f"\nO {self.get_nome()} usou {self.get_habilidade()} no {alvo.get_nome()} e causou: {dano} de dano! \n")
   
 class Inimigo(Personagem):
   
@@ -58,8 +74,28 @@ class Jogo:
       print("\n Detalhes dos Personagens: ")
       print(self.heroi.exibir_detalhes())
       print(self.inimigo.exibir_detalhes())
-      input("Tecle Enter para iniciar a batalha...")
-      escolha = int(input("Escolha (1 - Ataque Normal, 2 Ataque Especial): "))
+      str(input("Tecle Enter para iniciar a batalha..."))
+      escolha = input("Escolha (1 - Ataque Normal, 2 Ataque Especial): ")
+
+      
+      if escolha == "1":
+        self.heroi.atacar(self.inimigo)
+      elif escolha == "2":
+        self.heroi.ataque_especial(self.inimigo)
+      elif escolha == "0":
+        break
+      else:
+        print("Escolha inválida")
+
+      if self.inimigo.get_vida() > 0:
+        self.inimigo.atacar(self.heroi)
+
+    if self.heroi.get_vida() > 0:
+      print(f"Parabéns, você venceu...")
+    else:
+      print(f"O Heroi está morto...")
+
+
 
 jogo = Jogo()
 jogo.iniciando_batalha()
